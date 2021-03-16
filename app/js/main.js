a43ef3ed5f0849fc4e8ac7f81c0900bd32c9c9f8
1-aziat-1 <as.ac2727@mail.ru>
2021-03-16 21:41:09 +1000

    shop 20

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,42 @@
 $(function () {
+
+  $('.shop__content-btn').on('click',function (){
+    $('.shop__content-btn').removeClass('shop__content-btn--active');
+    $(this).addClass('shop__content-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list')
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list')
+  });
+
+  $(function () {
+
+    $(".star").rateYo({
+      rating: 3.6,
+      starWidth: "12px",
+      readOnly: true
+    });
+
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text('$' + data.from);
+      $('.filter-price__to').text('$' + data.to);
+    },
+    onChange: function (data){
+      $('.filter-price__from').text('$' + data.from);
+      $('.filter-price__to').text('$' + data.to);
+    },
+  });
+
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
